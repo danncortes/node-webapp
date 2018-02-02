09edb37a4d0c8a95dae9350d015be684906b2590
danielcortes <dcortes@velocitypartners.net>
2018-02-02 11:57:25 -0500

    starting

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+//Middleware
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Erros appending server.log file');
+        }
+    });
+    next();
+});
+
+// Uncomment in case to activate maintenance view
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+//Helper - You can create functions to access it globally
+hbs.registerHelper('currentYear', () => new Date().getFullYear());
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        title: 'Home Page!',
+        text: 'This is a text of home page!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        title: 'About Page'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('App started at port 3000');
+});
